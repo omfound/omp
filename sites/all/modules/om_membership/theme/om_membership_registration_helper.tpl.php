bef687b131c73340b963ce58570236d56071381a
aampere <aampere@aamperes-MacBook-Pro.local>
2012-11-19 16:23:20 -0700

    Removing eroneous template.

--- sites/all/modules/om_membership/theme/om_membership_registration_helper.tpl.php
+++
@@ -1,9 +0,0 @@
-<ul>
-  <?php if (!empty($previous)): ?>
-    <li><?php print $previous; ?></li>
-  <?php endif; ?>
-  <?php if (!empty($next)): ?>
-    <li><?php print $next; ?></li>
-  <?php endif; ?>
-    <li>Step <?php print $current_step; ?> of <?php print $step_count; ?>: <?php print $step_label; ?></li>
-</ul>
