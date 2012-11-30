0d2ec8c90dc00ebfe9eee052bcc7b6d4eeeda1af
aampere <aampere@aamperes-MacBook-Pro.local>
2012-11-30 16:33:52 -0700

    Started adding renewal workflow.

---
+++ sites/all/modules/om_membership/theme/om_membership_renewal_table.tpl.php
@@ -0,0 +1,13 @@
+<div id="main-options">
+  <div id="current-plan">
+    <p>Your membership is about to expire. Continue enjoying all the benefits you receive from your membership in the future.</p>
+    <?php print l('Renew Now!', 'membership/renew-same-level'); ?>
+  </div>
+  <div id="upgrade-plan">
+    <p>Upgrade your membership today and enjoy all these added benefits:</p>
+    <?php print l('Upgrade Now!', 'membership/plans'); ?>
+  </div>
+</div>
+<div id="downgrade">
+  <p>Or contact station personnel to downgrade your plan.<p>
+</div>
