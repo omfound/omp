996abdba272d67929d08f0bd7c986f248e609a9b
Bart Lantz <bart@openmediafoundation.org>
2012-12-17 10:42:50 -0700

    added local settings.php

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -210,7 +210,21 @@
  *   );
  * @endcode
  */
-$databases = array();
+$databases = array (
+  'default' => 
+  array (
+    'default' => 
+    array (
+      'database' => 'openmedia',
+      'username' => 'root',
+      'password' => 'root',
+      'host' => 'localhost',
+      'port' => '8889',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
 
 /**
  * Access control for update.php script.
@@ -242,7 +256,7 @@ $update_free_access = FALSE;
  *   $drupal_hash_salt = file_get_contents('/home/example/salt.txt');
  *
  */
-$drupal_hash_salt = '';
+$drupal_hash_salt = 't1A6gVZayArsr4sLv2wwqLlae1lKC-_hlLwlMiv7uqo';
 
 /**
  * Base URL (optional).
