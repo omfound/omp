571f9a73549c4c80316ec8305ee38ba03222f1af
Brian Hiatt <brian@openmediafoundation.org>
2013-02-14 15:18:20 -0700

    theming for all video fields

--- sites/all/themes/openmedia/templates/field--field_om_show_video.tpl.php
+++ sites/all/themes/openmedia/templates/field--field_om_show_video.tpl.php
@@ -44,14 +44,25 @@
  * @ingroup themeable
  */
 ?>
-<div class="<?php print $classes; ?>"<?php print $attributes; ?>>
-  <?php dsm($items); ?>
-  <?php if (!$label_hidden): ?>
-    <div class="field-label"<?php print $title_attributes; ?>><?php print $label ?>:&nbsp;</div>
-  <?php endif; ?>
-  <div class="field-items"<?php print $content_attributes; ?>>
-    <?php foreach ($items as $delta => $item): ?>
-      <div class="field-item <?php print $delta % 2 ? 'odd' : 'even'; ?>"<?php print $item_attributes[$delta]; ?>><?php print render($item); ?></div>
-    <?php endforeach; ?>
+<?php if(count($items) > 0) { ?>
+  <?php drupal_add_js(libraries_get_path('jwplayer') . '/jwplayer.js'); ?> 
+  <div class="<?php print $classes; ?>"<?php print $attributes; ?>>
+    <?php if (!$label_hidden): ?>
+      <div class="field-label"<?php print $title_attributes; ?>><?php print $label ?>:&nbsp;</div>
+    <?php endif; ?>
+    <div class="field-items"<?php print $content_attributes; ?>>
+      <?php foreach ($items as $delta => $item): ?>
+        <div class="field-item <?php print $delta % 2 ? 'odd' : 'even'; ?>"<?php print $item_attributes[$delta]; ?>>
+          <div id="jwplayer-<?php print $delta; ?>">Loading video...</div>
+          <script type="text/javascript">
+            jwplayer("jwplayer-<?php print $delta; ?>").setup({
+              file: "<?php print $item['#markup']; ?>",
+              height: 360,
+              width: 640
+            });
+          </script>
+        </div>
+      <?php endforeach; ?>
+    </div>
   </div>
-</div>
+<?php } ?>
