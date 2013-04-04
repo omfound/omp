525ce639c3aa128e9b28850b09a54c6315daa759
Brian Hiatt <brian@openmediafoundation.org>
2013-04-04 10:25:38 -0600

    overriding all js in here

--- sites/all/themes/openmedia/js/om_reservations.js
+++ sites/all/themes/openmedia/js/om_reservations.js
@@ -39,12 +39,46 @@
         $(rightContent).fadeIn(1000);
         $(body).fadeIn(1000);
 
-        /**
         $('.fullcalendar .fc-content').unbind().mouseup(function(){
-            $('#date-div').append(addToCart);
-            $('#date-div').show();
-            console.log("!");
-        });**/
+          view = $('.fullcalendar').fullCalendar('getView');
+          if (view.name == 'agendaWeek'){
+            quantity = $('#selected-product select#edit-quantity').val();
+          $('#date-div').append(addToCart);
+          $('#date-div').show();
+          //$('#selected-product .commerce-add-to-cart .form-submit').show();
+	        $('#selected-product .commerce-add-to-cart').show();
+	        $('#selected-product div[id^="edit-line-item-fields"]').show();
+	        $('#selected-product .field-name-field-reservation-dates .form-select').change(function(){
+              startYear = $('#selected-product .start-date-wrapper .date-year .form-select').val();
+              startMonth = $('#selected-product .start-date-wrapper .date-month .form-select').val();
+              //fullcalendar select option is expecting a 0 based month array
+              startMonth = parseInt(startMonth) - 1;
+              startDay = $('#selected-product .start-date-wrapper .date-day .form-select').val();
+              if ($('#selected-product .start-date-wrapper .date-ampm .form-select').val() == 'pm'){
+                startHour = $('#selected-product .start-date-wrapper .date-hour .form-select').val();
+                startHour = parseInt(startHour) + 12;
+              } else{
+                startHour = $('#selected-product .start-date-wrapper .date-hour .form-select').val();
+              }
+              startMinutes = $('#selected-product .start-date-wrapper .date-minute .form-select').val();
+              endYear = $('#selected-product .end-date-wrapper .date-year .form-select').val();
+              endMonth = $('#selected-product .end-date-wrapper .date-month .form-select').val();
+              //fullcalendar select option is expecting a 0 based month array
+              endMonth = parseInt(endMonth) - 1;
+              endDay = $('#selected-product .end-date-wrapper .date-day .form-select').val();
+              if ($('#selected-product .end-date-wrapper .date-ampm .form-select').val() == 'pm'){
+                endHour = $('#selected-product .end-date-wrapper .date-hour .form-select').val();
+                endHour = parseInt(endHour) + 12;
+              } else{
+                endHour = $('#selected-product .end-date-wrapper .date-hour .form-select').val();
+              }
+              endMinutes = $('#selected-product .end-date-wrapper .date-minute .form-select').val();
+              startDate = new Date(startYear, startMonth, startDay, startHour, startMinutes, '00', '00');
+              endDate = new Date(endYear, endMonth, endDay, endHour, endMinutes, '00', '00');
+              $('.fullcalendar').fullCalendar('select', startDate, endDate, false);
+            });
+          }
+        });
 
       });
     }
