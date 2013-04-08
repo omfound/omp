7add39bef8f53f30c8dcba00bb26b3fd2db61d9f
Brian Hiatt <brian@openmediafoundation.org>
2013-04-08 17:26:24 -0600

    stillll fail

--- sites/all/themes/openmedia/template.php
+++ sites/all/themes/openmedia/template.php
@@ -482,7 +482,7 @@ function openmedia_preprocess_views_view_unformatted($vars) {
 function openmedia_preprocess_views_view_fields(&$vars) {
   $view = $vars['view'];
   if ($view->name == 'show_grid') {
-    dsm($vars['fields']['field_show_thumbnail']->raw);
+    dsm($vars['fields']['field_show_thumbnail']->content);
   }
   dsm($view->name);
 }
