26b0bdb8ac3607f994d0b103409e7602397ffe4b
Brian Hiatt <brian@openmediafoundation.org>
2013-04-08 17:46:19 -0600

    typo

--- sites/all/themes/openmedia/template.php
+++ sites/all/themes/openmedia/template.php
@@ -484,7 +484,7 @@ function openmedia_preprocess_views_view_fields(&$vars) {
   if ($view->name == 'show_grid') {
     if(strpos($vars['fields']['field_show_thumbnail']->content, 'no_image.jpg') !== false) {
       $image = '<img typeof="foaf:Image" src="http://dev.openmedia.gotpantheon.com/sites/default/files/styles/220x135/public/default_images/no_image.jpg" width="220" height="135" alt="success" />';
-      $url = l($image, 'node/'.$vars['fields']['field_show_thumbnai']->raw, array('html' => true));
+      $url = l($image, 'node/'.$vars['fields']['field_show_thumbnail']->raw, array('html' => true));
       $content = '<div class="field-content">';
       $content .= $url;
       $content .= '</div>';
