3b4df799f307ca3699c07334553faa63bf1d2c75
Brian Hiatt <brian@openmediafoundation.org>
2013-04-09 09:22:06 -0600

    correcting name for url

--- sites/all/themes/openmedia/template.php
+++ sites/all/themes/openmedia/template.php
@@ -490,7 +490,7 @@ function openmedia_preprocess_views_view_fields(&$vars) {
       $content .= $url;
       $content .= '</div>';
       $vars['fields']['field_show_thumbnail']->content = $content;
-      dsm($vars['fields']['field_om_show']->content);
+      dsm($vars['fields']['field_om_show_video']->content);
     }
   }
 }
