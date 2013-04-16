656dfd1c45d14876197525d4886075b51a1c8be7
Brian Hiatt <brian@openmediafoundation.org>
2013-04-16 17:25:37 -0600

    adding reservation orders template

---
+++ sites/all/themes/openmedia/templates/views-row/views-view-fields--reservation-orders.tpl.php
@@ -0,0 +1,56 @@
+<?php $checkedout = FALSE;?>
+<?php if ($row->field_field_checkout_status[0]['raw']['value'] == "Overdue"):?>
+  <div class = "reservation-overdue">
+<?php else:?>
+  <div class = "reservation">
+<?php endif;?>
+  <div class="reservation-dates">
+    <?php print $fields['field_reservation_dates']->content;?>
+  </div>
+  <table class="reservation-info-table">
+  <tr>
+  <th class = "item-title">Item</th>
+  <th class="user-title">Username</th>
+  <th class="order-title">Order Information</th>
+  <th class = "checkout-status-title">Checkout Status:</th>
+  <th></th>
+  </tr>
+  <tr>
+    <td class="item-name">
+      <?php print $fields['line_item_title']->content . '</br>';?>
+    </td>
+    <td class = "user-info">
+      <?php $user = user_load($fields['uid']->raw);?>
+      <?php print '<a href = "../user/' . $user->uid . '">' . $user->field_user_contact_info['und'][0]['first_name'] . ' ' . $user->field_user_contact_info['und'][0]['last_name'] . '</a></br>';?>
+    </td>
+    <td class = "order-link">
+      <?php print $fields['view_order']->content;?>
+    </td>
+    <td class="checkout-status">
+      <?php if ($row->field_field_checkout_status[0]['raw']['value'] == "Overdue"){?>
+        <div class = "checkout_status_bad">
+        <?php print $fields['field_checkout_status']->content;?>
+        </div>
+      <?php } else{?>
+        <div class = "checkout_status_good">
+          <?php print $fields['field_checkout_status']->content;?>
+        </div>
+      <?php }?>
+      </td>
+      <td>
+      <?php if ($checkedout){?>
+        <?php print 'Checked Out Product: ' . $fields['commerce_product']->content;?>
+      <?php } else if($row->field_field_checkout_status[0]['raw']['value'] == 'Checked In'){?>
+        <?php print 'Checked In Product: ' . $fields['commerce_product']->content;?>
+      <?php }?>
+      <?php if ($row->field_field_checkout_status[0]['raw']['value'] == 'Awaiting Checkout'){?>
+        <?php print '<a class = "button checkout_button" href = "cr/res_checkout/' . $fields['line_item_id']->raw . '"' . '> Checkout</a>';?>
+        <?php $checkedout = FALSE;?>
+      <?php }?>
+      <?php if ($row->field_field_checkout_status[0]['raw']['value'] == 'Checked Out' || $row->field_field_checkout_status[0]['raw']['value'] == 'Overdue'){?>
+        <?php print '<a class = "checkin_button" href = "cr/res_checkin/' . $fields['line_item_id']->raw . '"' . '> Check In</a>';?>
+        <?php $checkedout = TRUE;?>
+      <?php }?>
+    </td>
+  </table>
+</div>
