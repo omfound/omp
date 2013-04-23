f5b7e6f57c6f5570947450cc18fad79cd33b5061
Brian Hiatt <brian@openmediafoundation.org>
2013-04-23 17:12:18 -0600

    removing annoying debug

--- sites/all/modules/custom/commerce_reservations/js/product_filter.js
+++ sites/all/modules/custom/commerce_reservations/js/product_filter.js
@@ -387,7 +387,6 @@
                 });
               },
               beforeHide: function(){
-                alert('closing and updating');
                 quantity = $('.qtip-contentWrapper [id|=edit-quantity]').val();
                 //$('#left-side .add-to-cart').remove();
                 tipContent = this.options.content.text[0].outerHTML;
