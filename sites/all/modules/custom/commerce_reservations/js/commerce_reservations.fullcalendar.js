c1ff1b96279203f9aa13fa4a649ffacb9c17700e
Brian Hiatt <brian@openmediafoundation.org>
2013-05-17 16:09:51 -0600

    jeez

--- sites/all/modules/custom/commerce_reservations/js/commerce_reservations.fullcalendar.js
+++ sites/all/modules/custom/commerce_reservations/js/commerce_reservations.fullcalendar.js
@@ -29,7 +29,7 @@ Drupal.fullcalendar.plugins.commerce_reservations = {
       } else if(view.name == 'agendaWeek'){
         eventCount = parseInt($('.fullcalendar').fullCalendar('clientEvents').length);
         console.log(eventCount);
-        if (!$('.fullcalendar').fullCalendar('clientEvents').length) {
+        //if (!$('.fullcalendar').fullCalendar('clientEvents').length) {
           var basePath = Drupal.settings.basePath;
 	        $.ajax(
           {url : basePath + 'closed_times/',
@@ -51,7 +51,7 @@ Drupal.fullcalendar.plugins.commerce_reservations = {
             });
           }
         });
-      }
+      //}
     }
     }
     options.selectable = true;
