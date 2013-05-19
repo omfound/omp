79e4cd719058a1d16645757147d086fc2af8425e
Brian Hiatt <brian@openmediafoundation.org>
2013-05-19 10:08:45 -0600

    happen before closures

--- sites/all/modules/custom/commerce_reservations/js/product_filter.js
+++ sites/all/modules/custom/commerce_reservations/js/product_filter.js
@@ -68,6 +68,7 @@ Drupal.cr = Drupal.cr || {};
 
       //Populate details pane and calendar with defaults
       Drupal.behaviors.product_filter.moveItemToDetails();
+      alert('bet this happens before closures');
       Drupal.behaviors.product_filter.addDateToCalendar();
 
       //The user has selected a time on the calendar
