c43961dcfc9019e5cf316d8be0b5313685fc30c6
Brian Hiatt <brian@openmediafoundation.org>
2013-05-22 13:59:37 -0600

    wow

--- sites/all/modules/custom/commerce_reservations/js/commerce_reservations.fullcalendar.js
+++ sites/all/modules/custom/commerce_reservations/js/commerce_reservations.fullcalendar.js
@@ -61,7 +61,7 @@ Drupal.fullcalendar.plugins.commerce_reservations = {
                 event.textColor = 'white';
                 dom_id: this.dom_id;
                 counter = counter + 1;
-                if (counter < 35) {
+                if (counter < 30) {
                   $(".fullcalendar").fullCalendar('renderEvent', event, false);
                 }
             });
