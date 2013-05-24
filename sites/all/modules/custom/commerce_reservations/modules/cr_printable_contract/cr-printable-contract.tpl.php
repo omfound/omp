338071e87acf111332883541f09140fb275c368e
Brian Hiatt <brian@openmediafoundation.org>
2013-05-24 13:36:09 -0600

    fix order date

--- sites/all/modules/custom/commerce_reservations/modules/cr_printable_contract/cr-printable-contract.tpl.php
+++ sites/all/modules/custom/commerce_reservations/modules/cr_printable_contract/cr-printable-contract.tpl.php
@@ -17,7 +17,7 @@ dsm($order);
         <h2><?php print variable_get('site_name', ''); ?> Equipment Rental Contract</h2>
 <?php print variable_get('cr_contract_header','');
         ?>
-        Order Date: <?php print date_format_date($order_date, "long") . '<br />'; ?>
+        Order Date: <?php print date_format_date($order->created, "long") . '<br />'; ?>
         Name: <?php print $username ?><br />
         Email: <?php print $email ?><br />
         Phone: <?php print $phone ?><br />
