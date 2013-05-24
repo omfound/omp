857ee21dde7954c9040007d13c426a20d11681bb
Brian Hiatt <brian@openmediafoundation.org>
2013-05-24 13:28:26 -0600

    feels bad code in template

--- sites/all/modules/custom/commerce_reservations/modules/cr_printable_contract/cr-printable-contract.tpl.php
+++ sites/all/modules/custom/commerce_reservations/modules/cr_printable_contract/cr-printable-contract.tpl.php
@@ -30,51 +30,50 @@ $logourl = theme_get_setting('logo_path', '');
             </tr>
           </thead>
           <tbody>
-          <?php
-  $discount = variable_get('cr_membership_discount', 1); 
-  $comcral_cost_total = 0;
-  $member_cost_total = 0;
-
+  <?php
   $even_odd = 'even';
   print '<pre>';
   print_r($items);
   print '</pre>';
   foreach ($items as $item) {
-
     $cost = money_format('%(#10n', $item->commerce_total['und'][0]['amount']/100);
     $rate = money_format('%(#10n', ($item->product->commerce_price['und'][0]['amount']/100)).' / '.$item->product->field_charge_by_the_['und'][0]['value'];
     $title = $item->product->title;
-
-    ?>
-            <tr class="<?php print $even_odd; ?>">
-              <td>
-                <div><?php print $title.$rate; ?> </div>
-                  <?php
-    }
-    // if
-
-    ?>
-              </td>
-              <?php //print_r($item_node);?>
-              <td><?php echo $cost; ?></td>
-            </tr>
-            <?php
+  ?>
+  <tr class="<?php print $even_odd; ?>">
+    <td>
+       <div><?php print $title.$rate; ?> </div>
+    </td>
+    <td><?php echo $cost; ?></td>
+  </tr>
+  <?php
     $even_odd = ($even_odd == 'even') ? 'odd' : 'even';
   }
-  // foreach
-  **/
   ?>
-          </tbody>
-          <tfoot>
-            <tr class="<?php echo $even_odd; ?>">
-              <th>Total</th>
-              <td>99<?php //echo money_format('%(#10n', $comcral_cost_total) ?></td>
-            </tr>
-          <tfoot>
-        </table>
-        <div id="boilerplate"><?php if (module_exists('token')) { echo token_replace(variable_get('cr_contract_boilerplate', ''), 'node', $node); }
-  else  { echo variable_get('cr_contract_boilerplate',''); } ?></div>
-        <div id="footer"><?php if (module_exists('token')) { echo token_replace(variable_get('cr_contract_footer', ''), 'node', $node); }
-  else  { echo variable_get('cr_contract_footer',''); } ?></div>
-      </div>
-    </body>
+  </tbody>
+  <tfoot>
+    <tr class="<?php echo $even_odd; ?>">
+      <th>Total</th>
+      <td>99<?php //echo money_format('%(#10n', $comcral_cost_total) ?></td>
+    </tr>
+  <tfoot>
+</table>
+<div id="boilerplate">
+  <?php 
+  if (module_exists('token')) { 
+    echo token_replace(variable_get('cr_contract_boilerplate', ''), 'node', $node); 
+  }else{ 
+    echo variable_get('cr_contract_boilerplate',''); 
+  }?> 
+</div>
+
+<div id="footer">
+  <?php 
+  if (module_exists('token')) { 
+    echo token_replace(variable_get('cr_contract_footer', ''), 'node', $node); 
+  }else{ 
+    echo variable_get('cr_contract_footer',''); 
+  }?>
+</div>
+</div>
+</body>
