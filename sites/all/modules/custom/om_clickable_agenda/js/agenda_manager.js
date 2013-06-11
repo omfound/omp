766b1280cf11327c7a3f0250e39674c38e98cf8d
Brian Hiatt <brian@openmediafoundation.org>
2013-06-11 15:58:29 -0600

    cmon o

--- sites/all/modules/custom/om_clickable_agenda/js/agenda_manager.js
+++ sites/all/modules/custom/om_clickable_agenda/js/agenda_manager.js
@@ -79,9 +79,9 @@ Drupal.agendaManger.Models.interpreter = Backbone.Model.extend({
     this.set('sessionBillsComplete', true);
   },
   addCuePoints : function(data) {
-    dataJSON = JSON.parse(data);
-    for (x in dataJSON) {
-      this.cuePointList.add(dataJSON[x]);
+    //dataJSON = JSON.parse(data);
+    for (x in data) {
+      this.cuePointList.add(data[x]);
     }
     this.updateCuePointListView();
     this.set('addCuePointsComplete', true);
