b8dc64a98d1ede4194da2578bd1ba8d659b10b67
Brian Hiatt <brian@openmediafoundation.org>
2013-06-13 15:40:52 -0600

    proper theme implementation for livestream info block

---
+++ sites/all/modules/custom/om_agenda_manager/theme/om-show-live-block.tpl.php
@@ -0,0 +1,7 @@
+<?php foreach ($live_themes as $theme) { ?>
+  <div id="om-show-live-block">
+    <div class="om-show-live-block-button">
+      <?php print $theme['button']; ?>
+    </div>
+  </div>
+<?php } ?>
