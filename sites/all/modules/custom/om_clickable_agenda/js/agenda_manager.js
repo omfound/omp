f8d260f08d8f3720b97e60229730d5ee7911225a
Leo Kacenjar <lkacenjar@dev.(none)>
2013-06-13 23:25:08 +0000

    Updating clickable agenda again.

--- sites/all/modules/custom/om_clickable_agenda/js/agenda_manager.js
+++ sites/all/modules/custom/om_clickable_agenda/js/agenda_manager.js
@@ -81,10 +81,10 @@ Drupal.agendaManger.Models.interpreter = Backbone.Model.extend({
     this.set('sessionBillsComplete', true);
   },
   addCuePoints : function(data) {
-    console.log(data);
     dataJSON = JSON.parse(data);
     for (x in dataJSON) {
       this.cuePointList.add(dataJSON[x]);
+      dsm(dataJSON[x]);
     }
     this.updateCuePointListView();
     this.set('addCuePointsComplete', true);
