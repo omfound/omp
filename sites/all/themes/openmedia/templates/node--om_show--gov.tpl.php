07dfcdca1f938ff99606f5b57cbdbc2751e4743b
Brian Hiatt <brian@openmediafoundation.org>
2013-06-14 16:26:42 -0600

    removing show left

--- sites/all/themes/openmedia/templates/node--om_show--gov.tpl.php
+++ sites/all/themes/openmedia/templates/node--om_show--gov.tpl.php
@@ -1,5 +1,4 @@
 <article class="node-<?php print $node->nid; ?> <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-  <div id="show-left">
     <?php if (isset($video)): ?>
       <?php print $video; ?>
     <?php endif; ?>
@@ -43,5 +42,4 @@
         <iframe src="<?php print $node->field_om_calendar[$node->language][0]['url']; ?>" width="100%" height="500" ></iframe> 
       </div>
     <?php } ?>
-  </div>
 </article><!-- /.node -->
