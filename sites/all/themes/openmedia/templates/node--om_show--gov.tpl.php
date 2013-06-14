dff06e8744b19c14ac4bb91b190035a66f7f3d50
Brian Hiatt <brian@openmediafoundation.org>
2013-06-14 15:48:06 -0600

    remove unnecessary elements for gov

--- sites/all/themes/openmedia/templates/node--om_show--gov.tpl.php
+++ sites/all/themes/openmedia/templates/node--om_show--gov.tpl.php
@@ -31,35 +31,7 @@
             <?php print $locally_produced; ?> | 
           </div>
         <?php endif; ?>
-        <div id="view-count" class="bold">
-          <?php print $view_count; ?> |
-        </div>
-        <div id="comment-count" class="bold">
-          <?php $comment_count = om_social_fb_comment_count(); ?>
-          <?php print $comment_count; ?> Comments
-        </div>
       </div>
-    </div>
-    <?php if (isset($description)): ?>
-      <div id="description" class="divider show-hide">
-        <div class="inner">
-          <?php print $description; ?>
-        </div>
-      </div>
-    <?php endif; ?>
-    <div id="voting" class="clearfix">
-      <div id="voting-msg">
-        <?php print $vote_message; ?>
-      </div>
-        <div id="voting-widget">
-          <?php if (!empty($vote_summary)) { ?>
-            <?php print $vote_summary; ?>
-          <?php } ?>
-          <?php print $vote_widget; ?>
-          <?php if (!empty($bayesian_score)): ?>
-            <?php print $bayesian_score; ?>
-          <?php endif; ?>
-        </div>
     </div>
     <div class="divider">
       <div class="inner">
@@ -67,9 +39,4 @@
       </div>
     </div>
   </div>
-  <?php if (isset($node_right)): ?>
-    <div id="show-right">
-      <?php print $node_right; ?>
-    </div>
-  <?php endif; ?>
 </article><!-- /.node -->
