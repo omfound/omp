66e79c879d767b2c8755df9495a17945bef3c919
Leo Kacenjar <lkacenjar@dev.(none)>
2013-06-17 23:44:03 +0000

    Adding debug to settings.

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -587,6 +587,7 @@ $conf['404_fast_html'] = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
 
 // Check for global OMP setting for SSL
 $secure_connection = variable_get('ssl_enabled', FALSE);
+print_r($conf);
 if (!empty($secure_connection)) {
   dsm('made it here');
   // Redirect to https:// and www if it's not there
