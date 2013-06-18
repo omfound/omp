3479ab8aaa5913d855010dc1e88e6f557b535b67
Leo Kacenjar <lkacenjar@dev.(none)>
2013-06-18 00:13:23 +0000

    Winning it to win it.

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -599,7 +599,8 @@ if (!empty($secure_connection)) {
   if (isset($_SERVER['PANTHEON_ENVIRONMENT']) && $_SERVER['PANTHEON_ENVIRONMENT'] === 'live') {
     if (!isset($_SERVER['HTTP_X_SSL']) || $_SERVER['HTTP_X_SSL'] != 'ON' || !is_numeric(stripos($_SERVER['HTTP_HOST'], 'www'))) {
       header('HTTP/1.0 301 Moved Permanently');
-      header('Location: https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+      $host = str_replace('www.', '', $_SERVER['HTTP_HOST']);
+      header('Location: https://www.' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
       exit(); 
     }
   }
