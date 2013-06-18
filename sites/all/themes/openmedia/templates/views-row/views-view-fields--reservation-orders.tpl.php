4bec2c278340e365b8287ffde03910df3aa73375
Brian Hiatt <brian@openmediafoundation.org>
2013-06-18 16:33:58 -0600

    add column header back

--- sites/all/themes/openmedia/templates/views-row/views-view-fields--reservation-orders.tpl.php
+++ sites/all/themes/openmedia/templates/views-row/views-view-fields--reservation-orders.tpl.php
@@ -12,7 +12,7 @@
   <th class="line-item-title">Item</th>
   <th class = "checkout-status-title">Status</th>
   <th class = "payment-status">Payment</th>
-  <th></th>
+  <th class = "checkout-options">Options</th>
   </tr>
   <tr>
     <td class="item-name">
