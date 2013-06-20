c876c8b0fb7d0b4fb8b692f47ffe6d540fa599aa
Brian Hiatt <brian@openmediafoundation.org>
2013-06-20 14:37:31 -0600

    custom width height for jdubs

--- sites/all/themes/openmedia/js/jwplayer-default.js
+++ sites/all/themes/openmedia/js/jwplayer-default.js
@@ -6,8 +6,8 @@
         if (typeof Drupal.settings.jwplayer[i].playlist !== "undefined") {
           jwplayer("jwplayer-"+i).setup({
             playlist: Drupal.settings.jwplayer[i].playlist,
-            height: 340,
-            width: 960,
+            height: Drupal.settings.jwplayer[i].height,
+            width: Drupal.settings.jwplayer[i].width,
             image: Drupal.settings.jwplayer[i].image,
             primary: "flash",
             listbar: {
@@ -19,7 +19,8 @@
         else{
           jwplayer("jwplayer-"+i).setup({
             file: Drupal.settings.jwplayer[i].path,
-            height: 340,
+            height: Drupal.settings.jwplayer[i].height,
+            width: Drupal.settings.jwplayer[i].width,
             image: Drupal.settings.jwplayer[i].image,
             width: 550 
           });
