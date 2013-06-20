1e62054fe8320b369bb551ecae8a5517fe99528b
Brian Hiatt <brian@openmediafoundation.org>
2013-06-20 15:42:23 -0600

    spacing

--- sites/all/themes/openmedia/templates/node--om_show--gov.tpl.php
+++ sites/all/themes/openmedia/templates/node--om_show--gov.tpl.php
@@ -6,6 +6,7 @@
 
     <?php if (!empty($node->field_om_calendar)) { ?>
       <div id="documents">
+        <br />
         <?php $edit_view = str_replace('/pub', '/edit', $node->field_om_calendar[$node->language][0]['url']); ?>
         <iframe src="<?php print $edit_view; ?>" width="100%" height="500" ></iframe> 
       </div>
