7b0acd53f4add22c35003d7a890db4f416582664
Brian Hiatt <brian@openmediafoundation.org>
2013-06-21 10:22:11 -0600

    magic

--- sites/all/themes/openmedia/templates/node--om_show--gov.tpl.php
+++ sites/all/themes/openmedia/templates/node--om_show--gov.tpl.php
@@ -7,8 +7,7 @@
     <?php if (!empty($node->field_om_calendar)) { ?>
       <div id="documents">
         <br />
-        <?php $edit_view = str_replace('/pub', '/pub?embedded=true', $node->field_om_calendar[$node->language][0]['url']); ?>
-        <?php //$edit_view = str_replace('/pub', '/edit?embedded=true&width=960', $node->field_om_calendar[$node->language][0]['url']); ?>
+        <?php $edit_view = str_replace('/pub', '/edit?rm=minimal', $node->field_om_calendar[$node->language][0]['url']); ?>
         <iframe src="<?php print $edit_view; ?>" width="100%" height="500" ></iframe> 
       </div>
     <?php } ?>
