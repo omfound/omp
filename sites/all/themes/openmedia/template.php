2e81d410e5d1746daca2e8daa21da7611657e36f
Brian Hiatt <brian@openmediafoundation.org>
2013-06-25 19:43:09 -0600

    url dsm

--- sites/all/themes/openmedia/template.php
+++ sites/all/themes/openmedia/template.php
@@ -83,6 +83,7 @@ function openmedia_preprocess_field(&$variables, $hook) {
 
 function openmedia_preprocess_field__field_om_show_video(&$variables) {
   $url = $variables['items'][0]['#markup'];
+  dsm($url);
   if (!empty($url)) {
     if ($youtube_id = om_show_youtube_id($url)) {
       $livestream_status = om_show_youtube_livestream_status($youtube_id); 
