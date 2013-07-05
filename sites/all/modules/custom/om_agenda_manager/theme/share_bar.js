2d344cf9c0aa64fc0bf9ab3ab0410bab404ab604
Leo Kacenjar <lkacenjar@dev.(none)>
2013-07-05 14:53:41 +0000

    Adding script file and library inclusion for share bar.

---
+++ sites/all/modules/custom/om_agenda_manager/theme/share_bar.js
@@ -0,0 +1,7 @@
+(function ($, Drupal, window, document, undefined) {
+Drupal.behaviors.shareBar = {
+  attach : function(context) {
+    alert('attached');
+  }
+}
+})(jQuery, Drupal, this, this.document);
