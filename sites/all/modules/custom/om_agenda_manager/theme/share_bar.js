5a3ab4e91c3108e0bc0659fc154eafb2bc8b1d0e
Leo Kacenjar <lkacenjar@dev.(none)>
2013-07-05 15:21:36 +0000

    Updating share_bar.js

--- sites/all/modules/custom/om_agenda_manager/theme/share_bar.js
+++ sites/all/modules/custom/om_agenda_manager/theme/share_bar.js
@@ -6,21 +6,39 @@ Drupal.shareBar.views = {};
 Drupal.shareBar.models = {};
 
 // Models:
-// Extremely simple model to act as storage.
-Drupal.shareBar.models.shareBar = Backbone.Model.extend(); 
+// Extremely simple model to act as storage for player state and various resources.
+Drupal.shareBar.models.shareBar = Backbone.Model.extend(
+  initialize : function() {
+    console.log(this.player);
+  }
+); 
 // Views:
 // View to handle most of the interaction for the sharebar.
 Drupal.shareBar.views.shareBar = Backbone.View.extend({
   initialize : function(player, domInterface) {
+    // Standard bindall for this view.
+    _.bindAll('initializeInterface', this);
+    // Instantiate new sharebar model.
+    // This model will automatically bind itself to the player object.
+    this.shareBarModel = new Drupal.shareBar.models.shareBar({'player' : player});
+    // Attach this view to the dom element.
+    this.setElement(domInterface);
+    this.initializeInterface();
+  }
+  initializeInterface : function() {
     
   }
 });
 
 Drupal.behaviors.shareBar = {
   attach : function(context) {
+   // NB: here I've hard coded ids for the tray and used the ambiguous player call.
+   // This is due to the previous "frameworking" and lack of selectability other than arbitrary id.
+   // For now this limits one tray to a page.
    $target = $('#session-video-embed-tray', context);
    player = jwplayer();
-   console.log(player);
+   // Initialize tray.
+   var tray = new Drupal.shareBar.views.shareBar(player, $target);
   }
 }
 })(jQuery, Drupal, this, this.document);
