66774247bfc541de62368f0abb3616fa2a84fe0c
Leo Kacenjar <lkacenjar@dev.(none)>
2013-07-05 15:10:24 +0000

    Updating share_bar.js

--- sites/all/modules/custom/om_agenda_manager/theme/share_bar.js
+++ sites/all/modules/custom/om_agenda_manager/theme/share_bar.js
@@ -1,7 +1,26 @@
 (function ($, Drupal, window, document, undefined) {
+
+// Namespace global containers
+Drupal.shareBar = {};
+Drupal.shareBar.views = {};
+Drupal.shareBar.models = {};
+
+// Models:
+// Extremely simple model to act as storage.
+Drupal.shareBar.models.shareBar = Backbone.Model.extend(); 
+// Views:
+// View to handle most of the interaction for the sharebar.
+Drupal.shareBar.views.shareBar = Backbone.View.extend({
+  initialize : function(player, domInterface) {
+    
+  }
+});
+
 Drupal.behaviors.shareBar = {
   attach : function(context) {
-    alert('attached');
+   $target = $('#session-video-embed-tray', context);
+   player = jwplayer();
+   console.log(player);
   }
 }
 })(jQuery, Drupal, this, this.document);
