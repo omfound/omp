c528aa10b9400e8dffaf854a2165396906e71ced
Leo Kacenjar <lkacenjar@dev.(none)>
2013-07-08 19:48:59 +0000

    Updating module pile.

--- sites/all/modules/custom/om_jwplayer/theme/share_page.js
+++ sites/all/modules/custom/om_jwplayer/theme/share_page.js
@@ -1,7 +1,7 @@
 (function ($, Drupal, window, document, undefined) {
-Drupal.behaviors.sharePage = {
-  attach : function(context) {
-    alert('included');
+  Drupal.behaviors.sharePage = {
+    attach : function(context) {
+      
+    }
   }
-}
 })(jQuery, Drupal, this, this.document);
