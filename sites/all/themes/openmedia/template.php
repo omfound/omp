09f8c757235eab57ed5ee3361df65cabda472ff0
Leo Kacenjar <lkacenjar@dev.(none)>
2013-07-08 22:45:16 +0000

    Commenting out theme call for share_bar.

--- sites/all/themes/openmedia/template.php
+++ sites/all/themes/openmedia/template.php
@@ -103,9 +103,9 @@ function openmedia_preprocess_field__field_om_show_video(&$variables) {
       //default jwplayer code
       om_show_jwplayer_include($variables);
       $video = '<div id="jwplayer-0">Loading video...</div>';
-      if (arg(2) != 'agenda_manager') {
-        $video .= theme('om_show_share_bar');
-      }
+      //if (arg(2) != 'agenda_manager') {
+        //$video .= theme('om_show_share_bar');
+      //}
     }
     $variables['video'] = $video;
   }
