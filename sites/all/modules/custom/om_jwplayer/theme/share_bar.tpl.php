7b7585981b0754ed16842ff8a8149e46b4887fd5
Leo Kacenjar <lkacenjar@dev.(none)>
2013-07-08 17:02:08 +0000

    Adding player module.

---
+++ sites/all/modules/custom/om_jwplayer/theme/share_bar.tpl.php
@@ -0,0 +1,37 @@
+<div id="session-video-embed-tray" class='embed-tray'>
+  <div class="inner">
+    <form class="clearfix">
+      <div class="section start-end">
+        <div class="group">
+          <label for="embedInPoint" class="in-point">Set In Point</label>
+          <input type='textfield' class="in-point" name='embedInPoint' />
+        </div>
+        <div class="group">
+          <label for="embedOutPoint" class="out-point">Set Out Point</label>
+          <input type='textfield' class="out-point" name='embedOutPoint' />
+        </div>
+      </div>
+      <div class="section width-height">
+        <div class="group">
+          <label for="width" class="width">Width:</label>
+          <input type='width' class="width" name='width' />
+        </div>
+        <div class="group">
+          <label for="height" class="height">Height:</label>
+          <input type='height' class="height" name='height' />
+        </div>
+      </div>
+      <div class="section embeds">
+        <div class='embed'>
+          <label for="embed" class="embed">Embed:</label>
+          <input type='textfield' class="embed" name='embed' />
+        </div>
+        <div class="facebook">
+          <label for="facebookLink" class="facebook">Link:</label>
+          <input type='textfield' class="facebook" name='facebookLink' />
+        </div>
+      </div>
+    </form>
+  </div>
+  <div class="show-hide">Share This +</div>
+</div>
