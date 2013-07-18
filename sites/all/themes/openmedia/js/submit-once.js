05eb0d05d3eb8513a86f886502c238582ff2adae
Leo Kacenjar <lkacenjar@dev.(none)>
2013-07-18 21:14:35 +0000

    Removing js folder from om_timeslot_scheduler and adding submit-once.js to theme.

---
+++ sites/all/themes/openmedia/js/submit-once.js
@@ -0,0 +1,17 @@
+(function ($, Drupal, window, document, undefined) {
+  Drupal.behaviors.submitOnce = {
+    attach : function(context) {
+      $('.submit-once', context).once('submit-once'
+        function() {
+          $(this).click(
+            function() {
+              $(this).attr('disabled', 'disabled');
+              var text = $(this).attr('data-submit-once');
+              $(this).val(text);
+            }
+          )
+        }
+      );
+    }
+  };    
+})(jQuery, Drupal, this, this.document);  
