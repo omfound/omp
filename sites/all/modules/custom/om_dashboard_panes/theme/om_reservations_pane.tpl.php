653b4d3c8d5c4b3f6102dd0919038ec4a22a67f9
Bart Lantz <underhilllabs@gmail.com>
2013-07-22 15:09:08 -0400

    adding items to reservation pane in dashvboard

--- sites/all/modules/custom/om_dashboard_panes/theme/om_reservations_pane.tpl.php
+++ sites/all/modules/custom/om_dashboard_panes/theme/om_reservations_pane.tpl.php
@@ -1,3 +1,6 @@
 <div id="classes-pane" class="pane pane-title-only">
   <h3 class="pane-title"><?php print $title; ?></h3>
+  <div class="inner">
+     <?php print $reservations_table; ?>
+  </div>
 </div>
