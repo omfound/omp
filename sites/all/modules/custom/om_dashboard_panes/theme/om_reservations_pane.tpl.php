869f6798d9a253754d0ba94c79eeab4725669931
Bart Lantz <underhilllabs@gmail.com>
2013-07-22 20:57:12 -0400

    debugging reservations pane

--- sites/all/modules/custom/om_dashboard_panes/theme/om_reservations_pane.tpl.php
+++ sites/all/modules/custom/om_dashboard_panes/theme/om_reservations_pane.tpl.php
@@ -1,6 +1,7 @@
 <div id="classes-pane" class="pane pane-title-only">
   <h3 class="pane-title"><?php print $title; ?></h3>
   <div class="inner">
+     <h3>yelp</h3>
      <?php print $reservations_output; ?>
   </div>
 </div>
