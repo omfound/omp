70c4dee6f6d97b127a539837da5ecf0b57d0b8c5
Brian Hiatt <brian@openmediafoundation.org>
2013-07-29 15:30:26 -0600

    debugging closed dates

--- sites/all/modules/custom/commerce_reservations/js/commerce_reservations.fullcalendar.js
+++ sites/all/modules/custom/commerce_reservations/js/commerce_reservations.fullcalendar.js
@@ -73,6 +73,7 @@ Drupal.fullcalendar.plugins.commerce_reservations = {
                 event.eventBorderColor = '#56a4da';
                 event.textColor = 'white';
                 dom_id: this.dom_id;
+                console.log(event);
                 $(".fullcalendar").fullCalendar('renderEvent', event, false);
               });
           }
