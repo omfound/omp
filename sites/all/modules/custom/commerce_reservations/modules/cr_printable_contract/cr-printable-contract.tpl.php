0a0ffb34760bf20da013c721cb124843e25e68a8
Brian Hiatt <brian@openmediafoundation.org>
2013-08-01 17:04:07 -0600

    so close to accessories

--- sites/all/modules/custom/commerce_reservations/modules/cr_printable_contract/cr-printable-contract.tpl.php
+++ sites/all/modules/custom/commerce_reservations/modules/cr_printable_contract/cr-printable-contract.tpl.php
@@ -39,6 +39,9 @@ $logourl = theme_get_setting('logo_path', '');
     $cost = money_format('%(#10n', $item->commerce_total['und'][0]['amount']/100);
     $rate = money_format('%(#10n', ($item->product->commerce_price['und'][0]['amount']/100)).' / '.$item->product->field_charge_by_the_['und'][0]['value'];
     $title = $item->product->title;
+    if (!empty($item->clean_accessories)) {
+      $title .= '<br /><span style="font-size: .7em;">'.$item->clean_accessories.'</span>';
+    }
   ?>
   <tr class="<?php print $even_odd; ?>">
     <td><div><?php print $title.$rate; ?></div></td>
