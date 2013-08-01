c32280ea9253e5bdafa8b54d12f3af7deb8b29ec
Brian Hiatt <brian@openmediafoundation.org>
2013-08-01 16:56:04 -0600

    adding accessories to contracts

--- sites/all/modules/custom/commerce_reservations/modules/cr_printable_contract/cr-printable-contract.tpl.php
+++ sites/all/modules/custom/commerce_reservations/modules/cr_printable_contract/cr-printable-contract.tpl.php
@@ -35,6 +35,7 @@ $logourl = theme_get_setting('logo_path', '');
   <?php
   $even_odd = 'even';
   foreach ($items as $item) {
+    dsm($item);
     $cost = money_format('%(#10n', $item->commerce_total['und'][0]['amount']/100);
     $rate = money_format('%(#10n', ($item->product->commerce_price['und'][0]['amount']/100)).' / '.$item->product->field_charge_by_the_['und'][0]['value'];
     $title = $item->product->title;
