fe6d588a14481620760ef8c80ad6f41e05fa488f
Bart Lantz <bartlantz@gmail.com>
2013-08-05 18:05:01 -0600

    added row[1][value] to content field to feed view template

--- sites/all/themes/openmedia/templates/views-view-row-rss--classes--feed.tpl.php
+++ sites/all/themes/openmedia/templates/views-view-row-rss--classes--feed.tpl.php
@@ -12,5 +12,5 @@
   <link><?php print $link?></link>
   <description><?php print $description; ?></description>
   <summary><?php print $description; ?></summary>
-  <content> <?php print dpm($row); ?> </content>
+  <content> <?php print $row[1]['value'] ?> </content>
 </item>
