4a8bda28574fccad33ffe9975a137d57ef4de74c
Bart Lantz <bartlantz@gmail.com>
2013-08-05 18:19:58 -0600

    added opening php tag

--- sites/all/themes/openmedia/templates/views-view-row-rss--classes--feed.tpl.php
+++ sites/all/themes/openmedia/templates/views-view-row-rss--classes--feed.tpl.php
@@ -1,15 +1,15 @@
-   <?
-   /**
-   * @file views-view-row-rss.tpl.php
-   * Default view template to display a item in an RSS feed.
-   *
-   * @ingroup views_templates
-   */
-  ?>
-  <item>
-    <title><?php print $title; ?></title>
-    <link><?php print $link?></link>
-    <description><?php print $description; ?></description>
-    <summary><?php print $description; ?></summary>
-    <content> <?php print $row->elements[1]['value']; ?></content>
+<?php
+/**
+ * @file views-view-row-rss.tpl.php
+ * Default view template to display a item in an RSS feed.
+ *
+ * @ingroup views_templates
+ */
+?>
+<item>
+  <title><?php print $title; ?></title>
+  <link><?php print $link?></link>
+  <description><?php print $description; ?></description>
+  <summary><?php print $description; ?></summary>
+  <content> <?php print $row->elements[1]['value']; ?></content>
 </item>
