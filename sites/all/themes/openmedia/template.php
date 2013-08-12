a5da95e46ce1f013c3e1bcfe19592a0e98f39513
Brian Hiatt <brian@openmediafoundation.org>
2013-08-12 17:03:31 -0600

    function naming

--- sites/all/themes/openmedia/template.php
+++ sites/all/themes/openmedia/template.php
@@ -558,6 +558,7 @@ function openmedia_preprocess_views_view(&$variables) {
   $sub_functions[] = __FUNCTION__ . '__' . $variables['view']->name;
   $sub_functions[] = __FUNCTION__ . '__' . $variables['view']->name . '__' . $variables['view']->current_display;
   foreach ($sub_functions AS $function) {
+    dsm($function);
     if (function_exists($function)) {
       $function($variables);
     }
