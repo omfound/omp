fdfa8fdd3b8eff9f423fd61cc7d434e2fbe9fc31
Brian Hiatt <brian@openmediafoundation.org>
2013-08-13 10:06:12 -0600

    testing row values after alter

--- sites/all/themes/openmedia/templates/views-row/views-view-fields--reservation-orders.tpl.php
+++ sites/all/themes/openmedia/templates/views-row/views-view-fields--reservation-orders.tpl.php
@@ -1,4 +1,5 @@
 <?php $checkedout = FALSE;?>
+<?php dsm($row); ?>
 <?php if (!empty($row->field_field_checkout_status[0]['raw']['value']) && $row->field_field_checkout_status[0]['raw']['value'] == "Overdue"):?>
   <div class = "reservation-overdue">
 <?php else:?>
