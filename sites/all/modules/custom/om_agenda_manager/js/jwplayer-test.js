e6705dc354d04b3a3ea08e40ce17f2d052f47a0f
Brian Hiatt <brian@openmediafoundation.org>
2013-08-15 10:00:56 -0600

    specifying youtube

--- sites/all/modules/custom/om_agenda_manager/js/jwplayer-test.js
+++ sites/all/modules/custom/om_agenda_manager/js/jwplayer-test.js
@@ -1,13 +1,13 @@
 (function ($, Drupal, window, document, undefined) {
   Drupal.behaviors.jwTest = {
     attach : function() {
-      var i;
       jwplayer.key="LbV5colrtkrBDNQUDLcd08vwBs7psJqbdJTVTYyxrAM=";
       var player = jwplayer("jwplayer-test").setup({ 
         playlist: 'http://dev-thornton.gotpantheon.com/node/29357/cuepoints',
         height: 500,
         width: 800,
         primary: "flash",
+        type: "youtube",
         listbar: {
           position: 'right',
           size: 300
