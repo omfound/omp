2cfceb86d8436e809fa2313ea4d5e17d59e9b1c8
Brian Hiatt <brian@openmediafoundation.org>
2013-08-15 09:25:37 -0600

    adding agenda manager test

---
+++ sites/all/modules/custom/om_agenda_manager/js/jwplayer-test.js
@@ -0,0 +1,19 @@
+(function ($, Drupal, window, document, undefined) {
+  Drupal.behaviors.jwTest = {
+    attach : function() {
+      var i;
+      jwplayer.key="LbV5colrtkrBDNQUDLcd08vwBs7psJqbdJTVTYyxrAM=";
+      var player = jwplayer("jwplayer-test").setup({ 
+        playlist: 'http://dev-thornton.gotpantheon.com/node/29357/cuepoints',
+        height: 500,
+        width: 800,
+        primary: "flash",
+        listbar: {
+          position: 'right',
+          size: 300
+        }
+      });
+    }
+  };
+})(jQuery, Drupal, this, this.document);
+
