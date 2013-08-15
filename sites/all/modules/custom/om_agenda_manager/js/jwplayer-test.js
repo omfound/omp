42f1f85e02fe9733d947b4ec523b61b6db81e4dc
Brian Hiatt <brian@openmediafoundation.org>
2013-08-15 17:01:52 -0600

    change to onplay

--- sites/all/modules/custom/om_agenda_manager/js/jwplayer-test.js
+++ sites/all/modules/custom/om_agenda_manager/js/jwplayer-test.js
@@ -13,15 +13,18 @@
         },
         events: {
           onPlaylistItem: function(event) {
-            var currentPlaylistItem = jwplayer().getPlaylistItem(event.index);
+            //var currentPlaylistItem = jwplayer().getPlaylistItem(event.index);
             //jwplayer().seek(currentPlaylistItem.mediaid);
             //console.log(currentPlaylistItem.mediaid);
             //console.log(event);
             //console.log(jwplayer().getPlaylistItem(event.index));
           },
           onPlay: function(event) {
-            console.log(event);
-            console.log(jwplayer().getPlaylistItem(event.index));
+            if (event.oldstate == "BUFFERING" && event.newstate == "PLAYING") {
+              var currentPlaylistItem = jwplayer().getPlaylistItem(event.index);
+              jwplayer().seek(currentPlaylistItem.mediaid);
+              //console.log(jwplayer().getPlaylistItem(event.index));
+            }
           }
         }
       });
