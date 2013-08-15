13cb8fe963cd808bfaa43e6d223a92df7f14813c
Brian Hiatt <brian@openmediafoundation.org>
2013-08-15 13:15:35 -0600

    switching to old flash

--- sites/all/modules/custom/om_agenda_manager/js/jwplayer-test.js
+++ sites/all/modules/custom/om_agenda_manager/js/jwplayer-test.js
@@ -3,6 +3,7 @@
     attach : function() {
       jwplayer.key="LbV5colrtkrBDNQUDLcd08vwBs7psJqbdJTVTYyxrAM=";
       var player = jwplayer("jwplayer-test").setup({ 
+        flashplayer:  'http://dev-thornton.gotpantheon.com/sites/all/modules/om_agenda_manager/js/jwplayer/player.swf',
         playlist: 'http://dev-thornton.gotpantheon.com/node/29357/cuepoints',
         height: 500,
         width: 800,
