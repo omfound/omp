e500edf30f9c7ae80f90ae181324f58be3becc93
Leo Kacenjar <lkacenjar@dev.(none)>
2013-08-30 18:44:02 +0000

    Updating upcoming airings all template.

---
+++ sites/all/modules/custom/om_show/theme/om_show_upcoming_airings_all.tpl.php
@@ -0,0 +1,8 @@
+<?php if (!empty($airings)): ?>
+  <table>
+    <thead><th>Show</th><th>Airing Date</th><th>Channel</th></thead>
+    <?php foreach ($airings AS $airing): ?>
+      <tr><td><?php print $airing['show']; ?></td>td><?php print $airing['airing']; ?></td>td><?php print $airing['channel']; ?></td></tr>
+    <?php endforeach; ?>
+  </table>
+<?php endif; ?>
