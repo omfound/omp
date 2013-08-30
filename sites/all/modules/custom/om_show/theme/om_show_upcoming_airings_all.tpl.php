3e1bb85648e3eeb265099a9e9eb6a6a090c7b169
Leo Kacenjar <lkacenjar@dev.(none)>
2013-08-30 19:02:59 +0000

    Updating om_show_upcoming_airings_all.tpl.php

--- sites/all/modules/custom/om_show/theme/om_show_upcoming_airings_all.tpl.php
+++ sites/all/modules/custom/om_show/theme/om_show_upcoming_airings_all.tpl.php
@@ -2,7 +2,7 @@
   <table>
     <thead><th>Show</th><th>Airing Date</th><th>Channel</th></thead>
     <?php foreach ($airings AS $airing): ?>
-      <tr><td><?php print $airing['show']; ?></td>td><?php print $airing['airing']; ?></td>td><?php print $airing['channel']; ?></td></tr>
+      <tr><td><?php print $airing['show']; ?></td><td><?php print $airing['airing']; ?></td><td><?php print $airing['channel']; ?></td></tr>
     <?php endforeach; ?>
   </table>
 <?php else: ?>
