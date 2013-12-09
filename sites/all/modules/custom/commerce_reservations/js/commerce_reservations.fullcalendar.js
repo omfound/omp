a7baca2c222c9b8566ef675b51a985f94535a7a9
Brian Hiatt <brian@openmediafoundation.org>
2013-12-09 14:01:23 -0700

    working on fix for all day res conflict

--- sites/all/modules/custom/commerce_reservations/js/commerce_reservations.fullcalendar.js
+++ sites/all/modules/custom/commerce_reservations/js/commerce_reservations.fullcalendar.js
@@ -95,7 +95,10 @@ Drupal.fullcalendar.plugins.commerce_reservations = {
       dateInvalid = false;
       dateDiff = Math.abs(end - start);
       dateDiffHours = dateDiff / (1000*60*60);
-
+      if (allDay) {
+        console.log(start);
+        console.log(end);
+      }
       if (!allDay){
         dontCheck = false;
         today = new Date();
