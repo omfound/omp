a4bad1d57ee0620bccec18e0ece69a10f2eaf6bf
Brian Hiatt <brian@openmediafoundation.org>
2014-03-03 09:43:53 -0700

    contract updates to include sku

--- sites/all/modules/custom/commerce_reservations/modules/cr_printable_contract/cr-printable-contract.tpl.php
+++ sites/all/modules/custom/commerce_reservations/modules/cr_printable_contract/cr-printable-contract.tpl.php
@@ -26,6 +26,7 @@ $logourl = theme_get_setting('logo_path', '');
           <thead>
             <tr>
               <th>Item</th>
+              <th>SKU</th>
               <th>Checkout Date</th>
               <th>Return Date</th>
               <th>Cost</th>
@@ -38,6 +39,7 @@ $logourl = theme_get_setting('logo_path', '');
     $cost = money_format('%(#10n', $item->commerce_total['und'][0]['amount']/100);
     $rate = money_format('%(#10n', ($item->product->commerce_price['und'][0]['amount']/100)).' / '.$item->product->field_charge_by_the_['und'][0]['value'];
     $title = $item->product->title;
+    $sku = $item->product->sku;
     $accessories = '';
     if (!empty($item->clean_accessories)) {
       $accessories = '<br /><strong>+</strong><span style="font-size: .8em;"> '.$item->clean_accessories.'</span>';
@@ -45,6 +47,7 @@ $logourl = theme_get_setting('logo_path', '');
   ?>
   <tr class="<?php print $even_odd; ?>">
     <td><div><?php print $title.$rate.$accessories; ?></div></td>
+    <td><?php print $sku; ?></td>
     <td><?php print date("F j, Y, g:i a", strtotime($item->field_reservation_dates['und'][0]['value'].' UTC')); ?></td>
     <td><?php print date("F j, Y, g:i a", strtotime($item->field_reservation_dates['und'][0]['value2'].' UTC')); ?></td>
     <td><?php echo $cost; ?></td>
