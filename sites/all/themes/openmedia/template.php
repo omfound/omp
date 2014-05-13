5c2c18f02d434719c7f7f624274adac87ba29dc1
martin <marty@openmediafoundation.org>
2014-05-13 10:45:56 -0600

    template.php

--- sites/all/themes/openmedia/template.php
+++ sites/all/themes/openmedia/template.php
@@ -325,6 +325,7 @@ function openmedia_preprocess_node__om_show(&$variables) {
 
 function openmedia_preprocess_node__om_project(&$variables) {
   dsm($variables);
+  $variables['project_title'] = $variables['title'];
   // Author Info
   // User picture
   if (isset($variables['picture']) && $variables['picture'] > 0) {
