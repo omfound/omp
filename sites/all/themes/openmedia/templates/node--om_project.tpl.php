6a675c5963af33f405e6f8094958f9c95547c7d3
martin <marty@openmediafoundation.org>
2014-05-14 10:10:03 -0600

    the fix is clear

--- sites/all/themes/openmedia/templates/node--om_project.tpl.php
+++ sites/all/themes/openmedia/templates/node--om_project.tpl.php
@@ -1,54 +1,56 @@
 <article class="node-<?php print $node->nid; ?> <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
   <?php if (isset($video)):?>
-  <div class="project-video">
-    <?php print $video; ?>
-  </div>
-  <?php endif; ?>
-  <div id="project-top" class="clearfix project-top">
-    <?php if (isset($project_title)): ?> 
-    <?php print $project_title; ?>
+  <div class="clearfix">
+    <div class="project-video">
+      <?php print $video; ?>
+   </div>
     <?php endif; ?>
-      <?php if (isset($description)): ?>
-        <div id="project-description">
-          <?php print $description; ?>
-        </div>
+    <div id="project-top" class="clearfix project-top">
+      <?php if (isset($project_title)): ?> 
+      <?php print $project_title; ?>
       <?php endif; ?>
-      <div id="project-metadata">
-        <?php if (isset($locally_produced)): ?>
-          <div id="project-locally-produced">
-            <div class="bold label">Production:</div>
-            <div class="value"><?php print $locally_produced; ?></div>
+        <?php if (isset($description)): ?>
+          <div id="project-description">
+            <?php print $description; ?>
           </div>
         <?php endif; ?>
-        <?php if (isset($rating)): ?>
-          <div id="project-rating">
-            <div class="bold label">Rating:</div>
-            <div class="value"><?php print $rating; ?></div>
-          </div>
-        <?php endif; ?>
-        <?php if (isset($project_language)): ?>
-          <div id="project-language">
-            <div class="bold label">Language:</div>
-            <div class="value"><?php print $project_language; ?></div>
-          </div>
-        <?php endif; ?>
-        <?php if (isset($genre)): ?>
-          <div id="project-genre">
-            <div class="bold label">Genre:</div>
-            <div class="value"><?php print $genre; ?></div>
-          </div>
-        <?php endif; ?>
-        <?php if (isset($theme)): ?>
-          <div id="project-theme">
-            <div class="bold label">Theme:</div>
-            <div class="value"><?php print $theme; ?></div>
-          </div>
-        <?php endif; ?>
+        <div id="project-metadata">
+          <?php if (isset($locally_produced)): ?>
+             <div id="project-locally-produced">
+               <div class="bold label">Production:</div>
+               <div class="value"><?php print $locally_produced; ?></div>
+             </div>
+          <?php endif; ?>
+          <?php if (isset($rating)): ?>
+            <div id="project-rating">
+              <div class="bold label">Rating:</div>
+              <div class="value"><?php print $rating; ?></div>
+            </div>
+          <?php endif; ?>
+          <?php if (isset($project_language)): ?>
+            <div id="project-language">
+              <div class="bold label">Language:</div>
+              <div class="value"><?php print $project_language; ?></div>
+            </div>
+          <?php endif; ?>
+          <?php if (isset($genre)): ?>
+            <div id="project-genre">
+              <div class="bold label">Genre:</div>
+              <div class="value"><?php print $genre; ?></div>
+            </div>
+          <?php endif; ?>
+          <?php if (isset($theme)): ?>
+            <div id="project-theme">
+              <div class="bold label">Theme:</div>
+              <div class="value"><?php print $theme; ?></div>
+            </div>
+          <?php endif; ?>
+        </div>
+        <div id="project-date-author">
+          <span class="bold"><?php print $created; ?> by <?php print $name; ?></span> - <?php print $view_count; ?>
+        </div>
       </div>
-      <div id="project-date-author">
-        <span class="bold"><?php print $created; ?> by <?php print $name; ?></span> - <?php print $view_count; ?>
-      </div>
-    </div>
+  </div>
     <?php if (isset($project_title)): ?>
     <div class="project-lower-title">
       <h2 class="more-from">MORE FROM <?php print $project_title; ?></h2>
