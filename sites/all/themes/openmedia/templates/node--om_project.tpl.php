904afe831bc2fa1760f52698b3bcc848fc0f52e4
martin <marty@openmediafoundation.org>
2014-05-19 13:10:25 -0600

    blarg

--- sites/all/themes/openmedia/templates/node--om_project.tpl.php
+++ sites/all/themes/openmedia/templates/node--om_project.tpl.php
@@ -15,6 +15,9 @@
           </div>
         <?php endif; ?>
         <div id="project-metadata">
+          <div id="project-date-author">
+-           <span class="bold"> Producer: <?php print $name; ?></span>
+-         </div>
           <?php if (isset($rating)): ?>
             <div id="project-rating">
               <div class="bold label">Rating:</div>
