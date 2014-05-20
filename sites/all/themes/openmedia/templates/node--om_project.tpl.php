62c78d95de2a1a551a834b4181cce3af59190139
martin <marty@openmediafoundation.org>
2014-05-20 15:31:41 -0600

    template change

--- sites/all/themes/openmedia/templates/node--om_project.tpl.php
+++ sites/all/themes/openmedia/templates/node--om_project.tpl.php
@@ -1,4 +1,5 @@
 <article class="node-<?php print $node->nid; ?> <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+  <?php print $node_right; ?>
   <?php if (isset($video)):?>
   <div class="clearfix">
     <div class="project-video">
