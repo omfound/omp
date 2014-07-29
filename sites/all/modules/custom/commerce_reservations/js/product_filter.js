91c1b4ea5cf2e5bb69653a47a9503b7b73bc682e
Brian Hiatt <brian@openmediafoundation.org>
2014-07-29 17:06:40 -0600

    fix for this

--- sites/all/modules/custom/commerce_reservations/js/product_filter.js
+++ sites/all/modules/custom/commerce_reservations/js/product_filter.js
@@ -261,7 +261,7 @@ Drupal.cr = Drupal.cr || {};
         cache : false,
         success : function (data) {
           counter = 0;
-          blockstatus = $('div.closed-dates-wrapper', data).attr('blocking');
+          blockstatus = $('div.closed-dates-wrapper', data).attr('data-blocking');
           console.log('block status: '+blockstatus);
           if (blockstatus == 'blocking') {
             event_class = 'unavailable-time-blocking';
