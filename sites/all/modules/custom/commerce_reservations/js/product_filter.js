06a8da0f3041e94c536de8449e0adc29da1a6ea7
Brian Hiatt <brian@openmediafoundation.org>
2014-07-29 17:22:26 -0600

    MAKE WORK

--- sites/all/modules/custom/commerce_reservations/js/product_filter.js
+++ sites/all/modules/custom/commerce_reservations/js/product_filter.js
@@ -262,7 +262,8 @@ Drupal.cr = Drupal.cr || {};
         success : function (data) {
           counter = 0;
           //blockstatus = $('div.closed-dates-wrapper', data).attr('data-blocking');
-          blockstatus = $('div[data-blocking="blocking"]', data);
+          //blockstatus = $('div[data-blocking="blocking"]', data);
+          blockstatus = $(data).find(‘div’).eq(0).data(‘blocking’);
           console.log('block status: '+blockstatus);
           if (blockstatus == 'blocking') {
             event_class = 'unavailable-time-blocking';
