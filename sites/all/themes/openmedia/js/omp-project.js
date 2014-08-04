ab00641ab4796d5ae95cf027e0f932825ae355ad
martin <marty@openmediafoundation.org>
2014-08-04 12:57:40 -0600

    wrong thing to add the class too

--- sites/all/themes/openmedia/js/omp-project.js
+++ sites/all/themes/openmedia/js/omp-project.js
@@ -3,7 +3,7 @@
     attach : function(context, settings) {
       console.log('this is my test');
        $('.read-more-button').click(function() { 
-         $('.read-more-button').addClass("active");
+         $('.project-description').addClass("active");
        });
     }
   }
