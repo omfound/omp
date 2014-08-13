b20f658890c1889fee913a8daa941463227f734e
Brian Hiatt <brian@openmediafoundation.org>
2014-08-13 12:30:43 -0600

    show page again

--- sites/all/modules/custom/om_show/theme/om_show_trending_page.tpl.php
+++ sites/all/modules/custom/om_show/theme/om_show_trending_page.tpl.php
@@ -1,1 +1,15 @@
-<h1>Temp</h1>
+<div class = "show-grid">
+  <?php foreach ($shows as $show) { ?>
+    <div class="show-preview">
+      <div class="show-image">
+        <img src="<?php print $show['thumbnail']; ?>" />
+      </div>
+      <div class="show-title">
+        <?php print $show['title']; ?>
+      </div>
+      <div class="show-votes">
+        Rating: <?php print $show['rating']; ?>
+      </div>
+    </div>
+  <?php } ?>
+</div>
