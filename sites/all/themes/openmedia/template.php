84fa87d9bef45ef632246b1698cc5ff124fcf6c8
martin <marty@openmediafoundation.org>
2014-08-19 16:40:29 -0600

    Te.

--- sites/all/themes/openmedia/template.php
+++ sites/all/themes/openmedia/template.php
@@ -359,7 +359,7 @@ function openmedia_preprocess_node__om_project(&$variables) {
   }
   // Body
   $variables['description'] = drupal_render($variables['content']['body']);
-  if (str_word_count($variables['content']['body']) > 70 ) {
+  if (str_word_count($variables['description']) > 70 ) {
     $variables['read_more'] = '<p class="read-more-button">Read More</p>';
   }
   // Local production
