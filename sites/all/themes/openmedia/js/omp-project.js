a314adddc47bb98354ff5677f9eec051fc3a3b2f
martin <marty@openmediafoundation.org>
2014-08-19 15:05:32 -0600

    remove console.log

--- sites/all/themes/openmedia/js/omp-project.js
+++ sites/all/themes/openmedia/js/omp-project.js
@@ -1,7 +1,6 @@
 (function ($, Drupal, window, document, undefined) {
   Drupal.behaviors.YourBehaviorName =  {
     attach : function(context, settings) {
-      console.log('this is my test');
        $('.read-more-button').click(function() { 
          $('#project-description .field-name-body').addClass("active");
        });
