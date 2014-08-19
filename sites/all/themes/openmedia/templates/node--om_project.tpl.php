1d47b6d7889af4c25e2a21722aa97beca069b124
martin <marty@openmediafoundation.org>
2014-08-19 16:01:36 -0600

    blah

--- sites/all/themes/openmedia/templates/node--om_project.tpl.php
+++ sites/all/themes/openmedia/templates/node--om_project.tpl.php
@@ -15,6 +15,9 @@
           <div id="project-description">
             <?php print $description; ?>
             <p class="read-more-button">Read More</p>
+            <?php if (isset($read_more)) {
+              print $read_more;
+            }
           </div>
         <?php endif; ?>
         <div id="project-metadata">
