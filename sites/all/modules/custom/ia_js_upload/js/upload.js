988e5a7b75b6a47494a43fdf6ab5c53b4c3cdf7c
Brian Hiatt <brian@openmediafoundation.org>
2014-08-25 16:00:04 -0600

    even closer to testing upload

---
+++ sites/all/modules/custom/ia_js_upload/js/upload.js
@@ -0,0 +1,36 @@
+(function($) {
+  Drupal.behaviors.evaporateUpload = {
+    attach: function(context) {
+      var evap = new Evaporate({
+        signerUrl: '/ia/sign',
+        aws_key: 'R7zRpLa4J2Gceu7k',
+        bucket: 'jstestbucket',
+      });
+
+      $('#files').change(function(evt){
+        files = evt.target.files;
+        for (var i = 0; i < files.length; i++){
+          evap.add({
+            name: 'test_' + Math.floor(1000000000*Math.random()),
+            file: files[i],
+            xAmzHeadersAtInitiate : {
+              'x-amz-acl': 'public-read'
+            },
+            signParams: {
+              foo: 'bar'
+            },
+            complete: function(){
+              console.log('complete................yay!');
+            },
+            progress: function(progress){
+              console.log('making progress: ' + progress);
+            }
+          });
+        }
+            
+        $(evt.target).val('');
+            
+      }); 
+    }
+  };
+}(jQuery));
