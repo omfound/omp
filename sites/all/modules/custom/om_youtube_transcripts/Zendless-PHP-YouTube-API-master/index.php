50b4868b14c22bab62fc3cddfd6db1591eb915eb
martin <marty@openmediafoundation.org>
2014-09-03 14:11:41 -0600

    index.php

--- sites/all/modules/custom/om_youtube_transcripts/Zendless-PHP-YouTube-API-master/index.php
+++ sites/all/modules/custom/om_youtube_transcripts/Zendless-PHP-YouTube-API-master/index.php
@@ -8,7 +8,7 @@
     $auth = new \API\google_oauth($key, $secret);
 
     //TODO: Be sure to alter the callback url below to reflect your environment
-    $data = $auth->get_request_token('http://dev-thornton.gotpantheon.com/authorize.php');
+    $data = $auth->get_request_token('http://dev-thornton.gotpantheon.com/sites/all/modules/custom/om_youtube_transcripts/Zendless-PHP-YouTube-API-master/authorize.php');
     // Store the returned token_secret in memory some where so
     // that it can be recalled during the authorization step.
     $_SESSION['token_secret'] = $data['token_secret'];
