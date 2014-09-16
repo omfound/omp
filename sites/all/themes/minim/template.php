c296f046aa457fbec379eb9a0e6ec494c8471a8c
Brian Hiatt <brian@openmediafoundation.org>
2014-09-16 14:47:20 -0600

    add blocks to base

--- sites/all/themes/minim/template.php
+++ sites/all/themes/minim/template.php
@@ -4,5 +4,6 @@
  */
 $theme_path = drupal_get_path('theme', 'minim');
 require_once($theme_path . '/includes/base.inc');
+require_once($theme_path . '/includes/blocks.inc');
 require_once($theme_path . '/includes/views.inc');
 require_once($theme_path . '/includes/components.inc');
