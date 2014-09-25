7ec28e02237bf8fae650c2f1faa6359fd4bf7912
Brian Hiatt <brian@openmediafoundation.org>
2014-09-25 09:53:26 -0700

    more seeking tests

--- sites/all/modules/custom/om_show/js/jwplayer-default.js
+++ sites/all/modules/custom/om_show/js/jwplayer-default.js
@@ -24,7 +24,7 @@
                     if (event.oldstate == "BUFFERING" && event.newstate == "PLAYING") {
                       // if we have an inpoint, do not seek to playlist the first time
                       if (Drupal.settings.jwplayer[0].embedInPoint) {
-                        console.log('seeking...');
+                        console.log('seeking: ' + Drupal.settings.jwplayer[0].embedInPoint);
                         player.seek(Drupal.settings.jwplayer[0].embedInPoint - 1);
                         Drupal.settings.jwplayer[0].embedInPoint = null;
                       }
