e5fc3e8ab4ddecf8a5d986f9f59676e96d971870
Brian Hiatt <brian@openmediafoundation.org>
2014-12-18 16:21:41 -0700

    more readmore efforts

--- sites/all/themes/openmedia/js/show_readmore.js
+++ sites/all/themes/openmedia/js/show_readmore.js
@@ -1,7 +1,7 @@
 (function ($, Drupal, window, document, undefined) {
   Drupal.behaviors.ShowReadMore =  {
     attach : function(context, settings) {
-      $('#description').readmore({
+      $('.field-name-body .field-item').readmore({
         maxHeight: 100,
         moreLink: '<a href="#">Read more &raquo;</a>'
       });
