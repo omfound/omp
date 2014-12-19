c8052db29fcfbec2566028ced3b8acf9f9a26c5b
Brian Hiatt <brian@openmediafoundation.org>
2014-12-19 13:29:19 -0700

    more debug

--- sites/all/themes/openmedia/template.php
+++ sites/all/themes/openmedia/template.php
@@ -823,7 +823,6 @@ function openmedia_preprocess_views_view_fields__show_grid(&$vars) {
 
 function openmedia_preprocess_views_view_fields__project_show_list(&$vars) {
   openmedia_improved_thumbnail($vars);
-  dsm($vars);
 }
 
 function openmedia_improved_thumbnail(&$vars) {
@@ -841,6 +840,7 @@ function openmedia_improved_thumbnail(&$vars) {
           $content .= $url;
           $content .= '</div>';
           $vars['fields']['field_show_thumbnail']->content = $content;
+          dsm($content);
         }
       }
     }
