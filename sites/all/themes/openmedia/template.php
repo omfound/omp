40b8acfb118acb7ebab42cde260d4a9844dcb562
Brian Hiatt <brian@openmediafoundation.org>
2014-12-22 10:01:54 -0700

    adding prep button and steps

--- sites/all/themes/openmedia/template.php
+++ sites/all/themes/openmedia/template.php
@@ -742,6 +742,8 @@ function openmedia_preprocess_views_view_fields__reservation_orders(&$variables)
   switch ($status) {
     case 'Awaiting Checkout':
       $link_options['attributes']['class'] = array('btn', 'btn-primary');
+      $variables['cr']['buttons'][] = l('Prep', 'cr/res_prep/' . $variables['row']->line_item_id, $link_options);
+      $link_options['attributes']['class'] = array('btn', 'btn-primary');
       $variables['cr']['buttons'][] = l('Check Out', 'cr/res_checkout/' . $variables['row']->line_item_id, $link_options);
       $link_options['attributes']['class'] = array('btn', 'btn-warning');
       $variables['cr']['buttons'][] = l('No Show', 'cr/res_noshow/' . $variables['row']->line_item_id, $link_options);
